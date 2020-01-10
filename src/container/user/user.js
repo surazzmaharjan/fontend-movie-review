4712fd5a06b5fc81ad73b6127ac2a4ec92780e23
Suraj Maharjan <surajmzn75@gmail.com>
2020-01-10 15:22:09 +0545

    setup container

---
+++ src/container/user/user.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import UserHeader from '../../components/common/header'
+import UserFooter from '../../components/common/footer'
+
+class User extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                    <UserHeader/>
+                    <UserFooter/>
+
+            </div>
+
+        )
+    }
+}
+
+export default User
+
